595c1d748d0ef683e5e88bf84da39e51801b2d0c
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2023-11-20 22:22:18 +0700

    server : register album api

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 const { registerPreResponseServerExtension } = require('./serverExtensions');
+const AlbumsService = require('./services/inMemory/AlbumsService');
+const albums = require('./api/albums');
+const AlbumsValidator = require('./validator/albums');
 
 const init = async () => {
+  const albumsService = new AlbumsService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -11,6 +15,14 @@ const init = async () => {
       cors: {
         origin: ['*'],
       },
+    },
+  });
+
+  await server.register({
+    plugin: albums,
+    options: {
+      service: albumsService,
+      validator: AlbumsValidator,
     },
   });
 
