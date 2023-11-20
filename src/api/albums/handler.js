99501648d7e1e1cf64a09530c465520e777297e6
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2023-11-20 22:21:53 +0700

    feat (album) : api create

---
+++ src/api/albums/handler.js
@@ -0,0 +1,30 @@
+const autoBind = require('auto-bind');
+
+class AlbumsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postAlbumHandler(request, h) {
+    this._validator.validateAlbumPayload(request.payload);
+    const { name = 'untitled', year } = request.payload;
+
+    const noteId = await this._service.addAlbum({ name, year });
+
+    const response = h.response({
+      status: 'success',
+      message: 'Album berhasil ditambahkan',
+      data: {
+        noteId,
+      },
+    });
+
+    response.code(201);
+    return response;
+  }
+}
+
+module.exports = AlbumsHandler;
