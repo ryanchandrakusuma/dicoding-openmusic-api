f451ef550b0d523774918780a0b85daf6e2b56d1
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2023-11-23 17:22:46 +0700

    migration : create songs table

---
+++ migrations/1700703346132_create-songs-table.js
@@ -0,0 +1,60 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    year: {
+      type: 'smallint',
+      notNull: true,
+    },
+    genre: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    performer: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    duration: {
+      type: 'smallint',
+      notNull: true,
+    },
+    album_id: {
+      type: 'VARCHAR(50)',
+      notNull: false,
+    },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'timestamp',
+      notNull: true,
+    },
+    deleted_at: {
+      type: 'timestamp',
+      notNull: false,
+    },
+  });
+
+  pgm.addConstraint('songs', 'fk_album_id', {
+    foreignKeys: {
+      columns: 'album_id',
+      references: 'albums(id)',
+      onDelete: 'CASCADE',
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
+};
