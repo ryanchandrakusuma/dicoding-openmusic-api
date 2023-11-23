b56745cf81a797c84c7846defef406c266dfc5ef
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2023-11-23 07:21:18 +0700

    migration : create albums table

---
+++ migrations/1700696933038_create-albums-table.js
@@ -0,0 +1,36 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('albums', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    year: {
+      type: 'smallint',
+      notNull: true,
+    },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'timestamp',
+      notNull: true,
+    },
+    deleted_at: {
+      type: 'timestamp',
+      notNull: false,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('albums');
+};
